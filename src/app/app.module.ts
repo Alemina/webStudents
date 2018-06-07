673425c24f85bae0aa80d5111c48c04cdd86efbd
Marcin Kobylinski <marcin.kobylinski@globallogic.com>
2018-06-07 10:26:27 +0200

    - added bootstrap - added some basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TopbarComponent } from './topbar/topbar.component';
+import { LeftSidebarComponent } from './left-sidebar/left-sidebar.component';
+import { RightSidebarComponent } from './right-sidebar/right-sidebar.component';
+import { MainFieldComponent } from './main-field/main-field.component';
+import { FooterComponent } from './footer/footer.component';
+import { CentralBarComponent } from './central-bar/central-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopbarComponent,
+    LeftSidebarComponent,
+    RightSidebarComponent,
+    MainFieldComponent,
+    FooterComponent,
+    CentralBarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/central-bar/central-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-central-bar',
+  templateUrl: './central-bar.component.html',
+  styleUrls: ['./central-bar.component.css']
+})
+export class CentralBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
